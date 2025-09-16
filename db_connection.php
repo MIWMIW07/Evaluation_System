5aa5f6ca949dea874914aa7fb67b143f216010df
Gab <learsi.gabriel07@gmail.com>
2025-09-16 11:58:24 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,4 @@
 <?php
-
-if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
-    header('Location: login.php');
-    exit;
-}
 // db_connection.php - PostgreSQL version
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -65,4 +60,5 @@ try {
     $conn = null;
 }
 ?>
+
 
