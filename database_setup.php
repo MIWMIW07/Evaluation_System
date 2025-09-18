f024f4a41aa084a49f4eed7efd451e4a3004bb70
Gab <learsi.gabriel07@gmail.com>
2025-09-18 11:18:24 +0800

    Update database_setup.php

--- database_setup.php
+++ database_setup.php
@@ -3,7 +3,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
+require_once 'includes/db_connection.php';
+require_once 'includes/security.php';
 
 echo "<!DOCTYPE html>
 <html><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'>
@@ -258,4 +259,5 @@ echo "</div>";
 
 echo "</div></body></html>";
 ?>
+
 
