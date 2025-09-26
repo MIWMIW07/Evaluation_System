17ee157d7fc55e98b19614a02a0f5ecc40dfced9
Gab <learsi.gabriel07@gmail.com>
2025-09-26 15:15:57 +0800

    Create healthcheck.php

---
+++ healthcheck.php
@@ -0,0 +1,9 @@
+<?php
+// healthcheck.php - Simple health check endpoint
+header('Content-Type: application/json');
+http_response_code(200);
+echo json_encode([
+    'status' => 'ok',
+    'timestamp' => date('Y-m-d H:i:s'),
+    'service' => 'Teacher Evaluation System'
+]);
