12ccd7d6a815b731d6f401d522ea161dcca1055c
Gab <learsi.gabriel07@gmail.com>
2025-09-26 15:47:53 +0800

    Delete healthcheck.php

--- healthcheck.php
+++
@@ -1,9 +0,0 @@
-<?php
-// healthcheck.php - Simple health check endpoint
-header('Content-Type: application/json');
-http_response_code(200);
-echo json_encode([
-    'status' => 'ok',
-    'timestamp' => date('Y-m-d H:i:s'),
-    'service' => 'Teacher Evaluation System'
-]);
