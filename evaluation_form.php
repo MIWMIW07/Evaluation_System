a01d428e31610908a843aa63b6a8a7ce2b763540
GLEN2004-TOQUE <144983795+GLEN2004-TOQUE@users.noreply.github.com>
2025-09-29 15:13:31 +0800

    Update evaluation_form.php

--- evaluation_form.php
+++ evaluation_form.php
@@ -126,13 +126,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !$is_view_mode && $teacher_info) {
 
         // Insert evaluation using the updated table structure
         $insert_sql = "INSERT INTO evaluations (
-            student_username, student_name, teacher_name, section, program,
-            q1_1, q1_2, q1_3, q1_4, q1_5, q1_6,
-            q2_1, q2_2, q2_3, q2_4,
-            q3_1, q3_2, q3_3, q3_4,
-            q4_1, q4_2, q4_3, q4_4, q4_5, q4_6,
-            comments, created_at, submitted_at
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+    student_username, student_name, teacher_name, section, program,
+    q1_1, q1_2, q1_3, q1_4, q1_5, q1_6,
+    q2_1, q2_2, q2_3, q2_4,
+    q3_1, q3_2, q3_3, q3_4,
+    q4_1, q4_2, q4_3, q4_4, q4_5, q4_6,
+    comments
+) VALUES (
+    ?, ?, ?, ?, ?,
+    ?, ?, ?, ?, ?, ?,
+    ?, ?, ?, ?,
+    ?, ?, ?, ?,
+    ?, ?, ?, ?, ?, ?,
+    ?
+)";
 
         $params = [
             $_SESSION['username'],
