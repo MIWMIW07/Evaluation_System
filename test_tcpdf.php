3d087207748ed6b67bd652a8c8b0e6f033e48aff
Gab <learsi.gabriel07@gmail.com>
2025-09-30 12:48:28 +0800

    Create test_tcpdf.php

---
+++ test_tcpdf.php
@@ -0,0 +1,10 @@
+<?php
+// test_tcpdf.php
+require_once 'tcpdf/tcpdf.php';
+
+$pdf = new TCPDF();
+$pdf->AddPage();
+$pdf->SetFont('helvetica', 'B', 16);
+$pdf->Cell(0, 10, 'TCPDF Test - Working!', 0, 1, 'C');
+$pdf->Output('test.pdf', 'I');
+?>
