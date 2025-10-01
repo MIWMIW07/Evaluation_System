94df2c0d7321cfacac56306c3f9f050ab46da223
Gab <learsi.gabriel07@gmail.com>
2025-10-01 13:28:24 +0800

    Update local_reports_generator.php

--- local_reports_generator.php
+++ local_reports_generator.php
@@ -15,7 +15,7 @@ try {
     
     // Check if TCPDF is available
     if (!class_exists('TCPDF')) {
-        require_once __DIR__ . '/vendor/tecnickcom/tcpdf/tcpdf.php';
+        require_once __DIR__ . 'tcpdf/tcpdf.php';
     }
     
     // TCPDF Class Extension
